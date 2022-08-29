b58d100c5236fe76d7afd79eb923da8afa764f3a
Myroslava123 <111084898+Myroslava123@users.noreply.github.com>
2022-08-29 12:43:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function enter(event) {
 
 function wyszukaj(city) {
   let apiKey = "991243b84955ce6858e2b1379960e636";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(wynik);
 }
 
